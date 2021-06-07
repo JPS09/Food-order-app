51ddb91c3703ea10633befeb49af4e5cd85a0327
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-06-07 11:20:02 +0200

    Added styling and alt on img

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,15 +1,15 @@
-import "./Header.module.css";
+import classes from "./Header.module.css";
 import mealsImage from "../Assets/meals.jpg"; // Importing the img to make it available in react
 const Header = (props) => {
   return (
     <>
-      <header>
+      <header className={classes.header}>
         <h1>ReactFood</h1>
         <button>Cart placeholder</button>
       </header>
-      <div>
+      <div className={classes["main-image"]}>
         {/* Providing the above import into the src attr */}
-        <img src={mealsImage} alt="" />
+        <img src={mealsImage} alt="A lot of delicious food" />
       </div>
     </>
   );
