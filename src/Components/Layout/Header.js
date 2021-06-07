6754bcb91e6dbb65f03b812eaa163af4f797a2bc
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-06-07 10:42:28 +0200

    started working on header

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,16 @@
+import "./Header.module.css";
+const Header = () => {
+  return (
+    <>
+      <header>
+        <h1>ReactFood</h1>
+        <button>Cart placeholder</button>
+      </header>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </>
+  );
+};
+
+export default Header;
