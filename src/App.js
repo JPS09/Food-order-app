cac169ac96449979d5e21567979e23f10c0efbf9
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-06-07 11:24:10 +0200

    Added header to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Header from "./Components/Layout/Header.js";
 function App() {
   return (
-    <div>
+    <>
+      <Header></Header>
       <h2>Let's get started!</h2>
-    </div>
+    </>
   );
 }
 
