fe145ac5114e12e50114986436abf11a5b0db246
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-06-08 15:59:23 +0200

    Adjusting id for the form

--- src/Components/Meals/MealItem/MealItemForm.js
+++ src/Components/Meals/MealItem/MealItemForm.js
@@ -1,8 +1,19 @@
 import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input.js";
 const MealItemForm = (props) => {
   return (
     <form action="" className={classes.form}>
-      <input type="text" />
+      <Input
+        label="Amount"
+        input={{
+          id: "Amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+ Add</button>
     </form>
   );
