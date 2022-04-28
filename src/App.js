bb4e840c92ca22f169270870b6faf6bd5afb58fe
Jean-Pierre Symoens <jpsymoens0@gmail.com>
2022-04-28 12:50:44 +0200

    Adding a Context Provider

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./Components/Layout/Header.js";
 import Meals from "./Components/Meals/Meals.js";
 import Cart from "./Components/Cart/Cart.js";
 import { useState } from "react";
+import CartProvider from "./store/CartProvider.js";
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
 
@@ -12,13 +13,13 @@ function App() {
     setCartIsShown(false);
   };
   return (
-    <>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler}></Header>
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
